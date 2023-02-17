94167c88e268bc049d830553413d7a308c45fd05
chenshaoxin <78350814@qq.com>
2023-02-17 15:53:42 +0800

    text/event-stream sse前后端demo编写

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-var http = require("http");
+const http = require("http");
+const moment = require("moment");
 
-http.createServer(function(req, res) {
-    if(req.url === '/createSse') {
-        res.writeHead(200, {
-            "Content-Type": "text/event-stream",
-            "Cache-Control": 'no-cache',
-            "Connection": 'keep-alive',
-            "Access-Control-Allow-Origin": '*'
-        })
-        res.write('retry: 10000\n')
-        res.write("event: connecttime\n");
-        res.write("data: " + (new Date()) + "\n\n");
+// request -> 请求对象
+// response
+const server = http.createServer((req, res) => {
+  if (req.url === "/sse") {
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+      "Access-Control-Allow-Origin": "*",
+    });
+    res.write("event: test\n");
+    res.write("event: my\n");
+    // res.write(`data: ${getDate()}\n`);
+    setInterval(() => {
+      res.write(`data: ${getDate()}\n\n`);
+    }, 1000);
+  }
+});
 
-        const interval = setInterval(function() {
-            res.write("event: test\n");
-            res.write("data: " + (new Date()) + "\n\n");
-        }, 3000)
-        req.connection.addListener("close", function () {
-            clearInterval(interval);
-        }, false);
-    }
-}).listen(9988, '127.0.0.1')
+function getDate() {
+  return moment().format("YYYY-MM-DD HH:mm:ss");
+}
+
+server.listen(8080);
